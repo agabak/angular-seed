cd0d92c46a7c27540d1430d130c4f0f91d4724d8
agabak <agaba_k@hotmail.com>
2014-06-29 10:48:30 -0500

    Update controllers.js

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,11 +1,14 @@
 'use strict';
 
 /* Controllers */
+(function(app){
+  
+  var myController = function($scopr){
+  
+   $scope.mydata = 2 + 4;
+  };
+  
+  app.controller('myController ', ['$scope', myController ]) 
+  
+  }(angular.module('myApp.controllers')))
 
-angular.module('myApp.controllers', [])
-  .controller('MyCtrl1', ['$scope', function($scope) {
-
-  }])
-  .controller('MyCtrl2', ['$scope', function($scope) {
-
-  }]);
